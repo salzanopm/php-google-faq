45ea7dce12968cd3bf3226b24ae84fa5400e03af
salzanopm <salzanopm@gmail.com>
2022-01-25 19:04:37 +0100

    first-version finish

--- index.php
+++ index.php
@@ -53,6 +53,31 @@ $db = [
         'Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, 
         lasciando a te il controllo.'
     ],
+    'Perché il mio account è associato a un paese?' => [
+        'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire 
+        due cose: 
+        <br> 
+        <br>
+        <ol>
+            <li>
+                La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del 
+                rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori 
+                tramite una delle due società seguenti:
+            </li>
+            <ol>
+                <li>
+                    Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione 
+                    europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+                </li>
+                <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+            </ol>
+            <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>    
+        </ol>
+        <br>
+        <br>
+        Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata 
+        che li offre o dal paese a cui è associato il tuo account.' 
+    ],
     'Stabilire il paese associato al tuo account' => 
         ['Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per 
         quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente 
@@ -142,11 +167,11 @@ $db = [
     </div>
     
     <main>
-        <?php foreach($db as $role => $people) { ?>
-            <h2><?php echo ucfirst($role); ?></h2>
+        <?php foreach($db as $question => $answers) { ?>
+            <h2><?php echo ucfirst($question); ?></h2>
             <br>
-            <?php foreach($people as $name) { ?>
-            <div><?php echo $name; ?></div>
+            <?php foreach($answers as $single_answer) { ?>
+            <div><?php echo $single_answer; ?></div>
             <br>
             <?php } ?>        
         <?php } ?>
