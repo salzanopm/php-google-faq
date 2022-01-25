b96277d7ea5eecf6cfb3a7ca1d7c93185ac6bffb
salzanopm <salzanopm@gmail.com>
2022-01-25 16:17:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domande frequenti - Privacy e termini - Google</title>
+    <!-- links  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.2.0/css/all.min.css" integrity="sha512-6c4nX2tn5KbzeBJo9Ywpa0Gkt+mzCzJBrE1RB6fmpcsoN+b/w/euwIMuQKNyUoU/nToKN3a8SgNOtPrbW12fug==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header-nav-wrapper">
+        <header>
+            <div class="logo-container">
+                <img src="./img/google-logo.png" alt="">  
+            </div>
+            <span class="span-logo">Privacy e termini</span> 
+            <div class="button-menu-container">
+                <i class="fas fa-ellipsis-v fa-lg"></i>
+                <i class="fas fa-ellipsis-v fa-lg"></i>
+                <i class="fas fa-ellipsis-v fa-lg"></i>
+                <div class="img-profile-container">
+                    <img class="img-profile" src="./img/profile.jpg" alt="">
+                </div> 
+            </div>
+        </header>  
+        <nav>
+            <ul class="nav-header">
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li>Domande frequenti</li>
+            </ul>
+        </nav>  
+    </div>
+    <hr>
+    <main>
+
+
+    </main>
+    
+        <footer>
+            <div class="footer-wrapper">
+                <ul class="info-container">
+                    <li>Google</li>
+                    <li>.</li>
+                    <li>Tutto su Google</li>
+                    <li>.</li>
+                    <li>Privacy</li>
+                    <li>.</li>
+                    <li>Termini</li>
+                </ul>
+                <div class="language-container">
+                    <i class="fas fa-comment-alt"></i>
+                    <select name="" id="language"> 
+                        <option value="italian">italiano</option>
+                        <option value="english">english</option>
+                    </select>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
